9aec4efcffda8b0fe639d3c4dfeeb1bd00f84336
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2018-02-11 10:16:03 -0600

    Inicio de la maqueta

---
+++ uno.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet/less" type="text/css" href="plantilla.less" />
+</head>
+
+<body>
+	<?php include("pie.php")?>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.3.1/less.min.js" type="text/javascript"></script>
+</body>
+</html>
