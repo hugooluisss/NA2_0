f93a513c70da15fb9d971f09e203fac535b04394
Orlando M <jorlandomtzm@gmail.com>
2018-02-25 18:12:23 -0600

    Diseño página dos

---
+++ dos.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Casa Hogar</title>
+	<link rel="stylesheet/less" type="text/css" href="less/plantilla.less" />
+</head>
+
+<body>
+
+	<?php include("menu2.php")?>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-9 ">
+				<div class="titulo">
+					<h1>CASA HOGAR</h1>
+				</div>
+				<div class="cotenido">
+					<h2>¿Qué es Lorem Ipsum?</h2>
+					<p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis magna et mi dictum cursus. Nullam bibendum lorem a lorem porttitor ultrices. Ut ac dolor eget sem ullamcorper dignissim eu at urna. Phasellus mollis ligula vitae feugiat scelerisque. Integer ut risus magna. Nam sed lobortis diam. Pellentesque fermentum pharetra tincidunt. Cras viverra fermentum magna, eget pellentesque mi consequat at. Integer nec elit quam. Cras vel erat et nulla tristique tempor. Nunc rhoncus gravida mi vitae pharetra. Morbi eu erat eu enim convallis consectetur. Nam malesuada mollis convallis. In laoreet odio vitae ante feugiat accumsan. Vivamus tincidunt sapien ac odio tempus luctus. Aliquam luctus urna sapien.</p>
+
+					<p>Suspendisse efficitur est eu eros tincidunt, a consequat metus tristique. Etiam venenatis iaculis lorem, venenatis fringilla mauris dapibus a. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed commodo mi non ex porta fringilla. Vivamus sollicitudin nunc at dui tincidunt, sed rhoncus elit pretium. Vestibulum maximus mi eu dui cursus placerat. Aliquam erat volutpat. Phasellus porta nisl quam. Vivamus finibus lorem magna, sit amet laoreet elit pharetra in.</p>
+
+					<p>Nullam vitae dui tortor. Morbi lacinia dolor sed risus convallis, nec volutpat enim luctus. Sed ut metus lectus. Suspendisse magna sapien, efficitur quis vulputate nec, congue vitae arcu. Mauris facilisis in augue eu faucibus. Vestibulum nisi massa, facilisis sit amet tincidunt eleifend, iaculis vitae ante. Nam semper nisl in turpis hendrerit, a suscipit tortor aliquet. Integer faucibus metus semper tempus varius. Pellentesque ornare, tellus vel ultricies consectetur, enim turpis dictum urna, et rhoncus arcu nisl at dui. Aliquam facilisis dui non velit rhoncus, sit amet tincidunt lacus congue. Cras eu lorem condimentum, euismod erat quis, vestibulum velit. Aenean sit amet tellus at nunc aliquam interdum ac ac felis. Cras cursus diam a dapibus rutrum. Donec auctor aliquet sem, quis luctus quam porttitor a.</p>
+
+					<p>Proin rutrum nec elit eu feugiat. Nunc dignissim, est eget volutpat vulputate, sapien libero ultrices orci, non suscipit odio metus eu quam. Maecenas iaculis varius imperdiet. Nunc facilisis enim sapien, eu dignissim metus venenatis vel. Phasellus ligula ligula, suscipit sit amet ligula vitae, consectetur tristique libero. Donec venenatis luctus nisl ac congue. Praesent sollicitudin blandit ullamcorper. Fusce neque nibh, dignissim in ipsum nec, feugiat pellentesque elit. Ut ac urna in tortor lobortis aliquet. Integer ex quam, scelerisque ac placerat sed, sodales vel sem. Pellentesque accumsan dui eleifend lobortis tempus. Suspendisse sit amet ultricies nunc, vitae tincidunt nisi. Quisque dignissim quis tortor sit amet condimentum. Quisque pellentesque, elit nec convallis molestie, arcu elit eleifend felis, nec pellentesque leo felis id massa. Sed viverra, lacus ac eleifend varius, lacus diam auctor mi, eu gravida massa massa eget sem. Sed varius velit lacus, eu maximus ipsum fringilla eu.</p>
+
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-3">
+				<h1>MEDIA:</h1>
+
+				<!-- INICIO ELEMENTO MEDIA -->
+				<div class="vgb">
+					<div id="koya_child_6" class="koya-komponent-binding">
+						<div class="stb ggb">
+							<div class="vgb" data-index="0">
+								<div id="koya_child_7" class="koya-komponent-binding">
+									<div class="on ym eh-lb" data-context-item-id="fuAE3U1NvOs">
+										<a href="/watch?v=fuAE3U1NvOs" onclick="return clk(this);" data-click-tracking-params="CBoQ/FoYACITCJrqlOqYwtkCFRo+Tgod+K4GUTIKZy1oaWdoLXJjaFoPRkV3aGF0X3RvX3dhdGNo" aria-hidden="false">
+											<div class="rm">
+												<div class="qm" style="background-image: url(https://i.ytimg.com/vi/fuAE3U1NvOs/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&amp;rs=AOn4CLDoMFNpgPMk9lvSdUwwH36CCbBQsA);"></div>
+												<div class="rn">
+													<span class="kkb" aria-label="3 minutos, 40 segundos">
+														<span style="">3:40</span>
+													</span>
+												</div>
+											</div>
+										</a>
+										<div class="wm">
+											<div class="tm">
+												<a href="/watch?v=fuAE3U1NvOs" onclick="return clk(this);" data-click-tracking-params="CBoQ/FoYACITCJrqlOqYwtkCFRo+Tgod+K4GUTIKZy1oaWdoLXJjaFoPRkV3aGF0X3RvX3dhdGNo" aria-label="Masego - Navajo | A COLORS SHOW - 3 minutos, 40 segundos - ver vídeo">
+													<div class="vm">
+														<span class="kkb">
+															<span style="">Masego - Navajo | A COLORS SHOW</span>
+														</span>
+													</div>
+													<div class="sm">
+														<ul>
+															<li>
+																<span class="kkb">
+																	<span style="">COLORS</span>
+																</span>
+															</li>
+															<li>
+																<span class="kkb">
+																	<span style="">8,6&nbsp;M visualizaciones</span>
+																</span>
+															</li>
+															<li>
+																<span class="kkb">
+																	<span style="">Hace 10 meses</span>
+																</span>
+															</li>
+														</ul>
+													</div>
+												</a>
+												<div class="xm">
+													<div id="koya_child_8" class="koya-komponent-binding">
+														<a href="/channel/UC2Qw1dzXDBAZPwS7zm37g8g" onclick="return clk(this);" data-click-tracking-params="CBoQ/FoYACITCJrqlOqYwtkCFRo+Tgod+K4GUQ==" aria-label="Ver canal">
+															<div class="hrb">
+																<img class="grb" src="https://yt3.ggpht.com/-tZxu1xN73MY/AAAAAAAAAAI/AAAAAAAAAAA/l7rr8wPve70/s68-c-k-no-mo-rj-c0xffffff/photo.jpg">
+															</div>
+														</a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div id="koya_child_16" class="koya-komponent-binding"></div>
+					</div>
+				</div>
+				<!-- FIN ELEMENTO MEDIA -->
+
+			</div>
+		</div>
+	</div>
+	<?php include("pie.php")?>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.3.1/less.min.js" type="text/javascript"></script>
+</body>
+</html>
